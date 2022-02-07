a203a34cd79b8995af4a2042437127288ecb5438
Jaren Borst <jarenborst@student.rocva.nl>
2022-02-07 12:58:23 +0100

    php opdracht 1.1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$student = array("naam"=>"Jaren", "achternaam"=>"Borst", "klass"=>"1C", "leeftijd"=>"17");
+
+echo $student ["naam"];
+echo "<br>";
+echo $student ["achternaam"];
+echo "<br>";
+echo $student ["klass"];
+echo "<br>";
+echo $student ["leeftijd"];
+?>
+
+</body>
+</html>
