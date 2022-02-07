a7a7f66752317d0d6aefc7eb00eb20b78dc4339b
Jaren Borst <jarenborst@student.rocva.nl>
2022-02-07 14:19:03 +0100

    opdracht 2.4 af

---
+++ 2-Loops/Opdracht-2.4/zendinginfo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+
+echo"<ul>";
+foreach($klassen as $value){
+    echo"<li>".$value."</li>";
+}
+echo"<ul>";
+
+echo"<br/>";
+echo"Ik wens jullie een fijne schooldag." ;
+
+?>
+
+</body>
+</html>
