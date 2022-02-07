f329818091b0268a80879613a229685a2aee60e0
Jaren Borst <jarenborst@student.rocva.nl>
2022-02-07 13:13:29 +0100

    opdracht 1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$persoon["voornaam"]   = "Jaren";
+$persoon["achternaam"] = "Borst";
+$persoon["leeftijd"]       = 17;
+$persoon["opleding"]   = "software developer";
+echo  $persoon['voornaam']. " " .  $persoon['achternaam'] . " is "  . $persoon['leeftijd'] . " jaar oud en is student op het ROC van Amsterdam aan de opleiding  "  . $persoon['opleding'];
+?>
+
+</body>
+</html>
