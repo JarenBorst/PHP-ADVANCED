b1ffb3f9893a301679fa8c4befbf1bd1ca2acbe7
Jaren Borst <jarenborst@student.rocva.nl>
2022-02-07 14:30:04 +0100

    2.5 af

---
+++ 2-Loops/Opdracht-2.5/loops-foreach-deel3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+
+$student  = array(
+    "voornaam"    => "Jan",
+    "achternaam"  => "Janssen",
+    "klas"        => "9A",
+    "Leeftijd"    => 17,
+    "Woonplaats"  => "Amstelveen"
+  );
+
+foreach($student as $keys => $value){
+    echo"$keys => $value";
+    echo"<br>";
+}
+
+?>
+
+</body>
+</html>
