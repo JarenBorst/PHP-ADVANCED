57c2ef1c8be9a84f8a3548baa32bd5056d2347dc
Jaren Borst <jarenborst@student.rocva.nl>
2022-02-08 09:38:00 +0100

    opdracht 3.1 af

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+
+<?php
+
+echo "voornaam is:".$_POST["voornaam"]; 
+echo "<br>";
+echo "achternaam is:".$_POST["achternaam"];
+echo "<br>";
+echo "klas is:".$_POST["klass"];
+echo "<br>";
+echo "leeftijd is:".$_POST["leeftijd"];
+echo "<br>";
+echo "adres is:".$_POST["adress"];
+echo "<br>";
+echo "plaatsnaam is:".$_POST["plaatsnaam"];
+echo "<br>";
+echo "favoriete muziek band is:".$_POST["x"];
+?>
+
+
+
+</body>
+</html>
